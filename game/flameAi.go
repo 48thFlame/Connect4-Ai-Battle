67e4bcb876aac2f3feaca1960a8e567b54bcb538
48thFlame <79379047+48thFlame@users.noreply.github.com>
2023-04-07 11:40:00 +0300

    Flame ai has util, and improvements

--- game/flameAi.go
+++ game/flameAi.go
@@ -28,22 +28,6 @@ type flameStats struct {
 
 var fStats = &flameStats{}
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	} else {
-		return b
-	}
-}
-
-func min(a, b int) int {
-	if a > b {
-		return b
-	} else {
-		return a
-	}
-}
-
 func FlameAiGetMove(game Connect4Game) (col int) {
 	possibleMoves := getOrderedMoves(game.Board)
 
@@ -51,12 +35,14 @@ func FlameAiGetMove(game Connect4Game) (col int) {
 
 	var depth int
 
-	if game.TurnNum < 3 {
+	if game.TurnNum < 6 {
 		depth = 7
-	} else if game.TurnNum < 14 {
+	} else if game.TurnNum < 12 {
 		depth = 8
 	} else if game.TurnNum < 20 {
 		depth = 9
+	} else if game.TurnNum < 30 {
+		depth = 12
 	} else {
 		depth = game.TurnNum
 	}
@@ -106,16 +92,6 @@ func FlameAiGetMove(game Connect4Game) (col int) {
 		fStats.posBroke)
 
 	return bestMove
-}
-
-func inSlice(s []int, a int) bool {
-	for _, obj := range s {
-		if obj == a {
-			return true
-		}
-	}
-
-	return false
 }
 
 func getOrderedMoves(board CBoard) []int {
@@ -191,172 +167,73 @@ func staticEval(board *CBoard) (score int) {
 }
 
 func getLocationScore(board *CBoard) (score int) {
-	score += getPlr1CenterScore(board)
-	score += getPlr2CenterScore(board)
-	score += getPlr1MiddleScore(board)
-	score += getPlr2MiddleScore(board)
-	return
+	centerPlr1, centerPlr2 := getCenters(board)
+	midPl1, midPlr2 := getMiddles(board)
+	score += (centerPlr1 - centerPlr2) * CCenterS
+	score += (midPl1 - midPlr2) * CMidS
+	return score
 }
 
-func getPlr1CenterScore(board *CBoard) int {
-	centers := 0
+func getCenters(board *CBoard) (centerPlr1, centerPlr2 int) {
 	for _, row := range board {
-		if row[cCenterCol] == CPlr1Max {
-			centers++
+		spot := row[cCenterCol]
+		if spot == CPlr1Max {
+			centerPlr1++
+		} else if spot == CPlr2Min {
+			centerPlr2++
 		}
 	}
 
-	return centers * CCenterS
+	return centerPlr1, centerPlr2
 }
 
-func getPlr2CenterScore(board *CBoard) int {
-	centers := 0
+func getMiddles(board *CBoard) (midPlr1, midPlr2 int) {
 	for _, row := range board {
-		if row[cCenterCol] == CPlr2Min {
-			centers--
+		spot1 := row[cMidCol1]
+		spot2 := row[cMidCol2]
+
+		if spot1 == CPlr1Max || spot2 == CPlr1Max {
+			midPlr1++
+		} else if spot1 == CPlr2Min || spot2 == CPlr2Min {
+			midPlr2++
 		}
 	}
 
-	return centers * CCenterS
-}
-
-func getPlr1MiddleScore(board *CBoard) int {
-	middles := 0
-	for _, row := range board {
-		if row[cMidCol1] == CPlr1Max {
-			middles++
-		}
-		if row[cMidCol2] == CPlr1Max {
-			middles++
-		}
-	}
-
-	return middles * CMidS
-}
-
-func getPlr2MiddleScore(board *CBoard) int {
-	middles := 0
-	for _, row := range board {
-		if row[cMidCol1] == CPlr2Min {
-			middles--
-		}
-		if row[cMidCol2] == CPlr2Min {
-			middles--
-		}
-	}
-
-	return middles * CMidS
+	return midPlr1, midPlr2
 }
 
 func getConnectionScore(board *CBoard) (score int) {
-	s := getCombinations(board)
-	score += getConnectionScoreForPlr1(s)
-	score += getConnectionScoreForPlr2(s)
+	b := *board
+	possibleMoves := getAvailableMoves(board)
+	for _, move := range possibleMoves {
+		b = cPutPieceOnBoard(b, move, CPlr(-1))
+	}
+	s := getCombinations(b)
+
+	number3Plr1, number2Plr1 := getConnectionsForPlr(s, CPlr1Max)
+	number3Plr2, number2Plr2 := getConnectionsForPlr(s, CPlr2Min)
+
+	score += (number3Plr1 - number3Plr2) * CWinnableConnect3S
+	score += (number2Plr1 - number2Plr2) * CWinnableConnect2S
 
 	return score
 }
 
-func getConnectionScoreForPlr1(s [][]CPlr) int {
-	score := 0
+func getConnectionsForPlr(s [][]CPlr, p CPlr) (number3s, number2s int) {
 	var winnable2, winnable3 bool
 
 	for _, combination := range s {
-		winnable3 = winnableN(combination, CPlr1Max, 3)
+		winnable3 = winnableN(combination, p, 3)
 		if winnable3 {
-			score += CWinnableConnect3S
+			number3s++
 			continue
 		}
 
-		winnable2 = winnableN(combination, CPlr1Max, 2)
+		winnable2 = winnableN(combination, p, 2)
 		if winnable2 {
-			score += CWinnableConnect2S
-			continue
+			number2s++
 		}
 	}
 
-	return score
-}
-
-func getConnectionScoreForPlr2(s [][]CPlr) int {
-	score := 0
-	var winnable2, winnable3 bool
-
-	for _, combination := range s {
-		winnable3 = winnableN(combination, CPlr2Min, 3)
-		if winnable3 {
-			score -= CWinnableConnect3S
-			continue
-		}
-
-		winnable2 = winnableN(combination, CPlr2Min, 2)
-		if winnable2 {
-			score -= CWinnableConnect2S
-			continue
-		}
-	}
-
-	return score
-}
-
-func getCombinations(board *CBoard) [][]CPlr {
-	/*
-		[[2 0 0 0 2 2 2]
-		 [1 1 1 0 1 0 1]
-		 [1 2 2 0 0 0 2]
-		 [2 1 1 1 1 1 1]
-		 [1 0 0 2 2 2 2]
-		 [0 0 1 1 1 1 2]]
-	*/
-	var combinations [][]CPlr
-
-	// row combinations
-	for _, row := range board {
-		for colI := 0; colI < 4; colI++ {
-			combination := row[colI : colI+4]
-			combinations = append(combinations, combination)
-		}
-	}
-
-	// col combinations
-	for col := 0; col < CColsNum; col++ {
-		for rowI := 0; rowI < 3; rowI++ {
-			combination := []CPlr{board[rowI][col], board[rowI+1][col], board[rowI+2][col], board[rowI+3][col]}
-			combinations = append(combinations, combination)
-		}
-	}
-
-	for rowI := 0; rowI < 3; rowI++ {
-		for colI := 0; colI < 4; colI++ {
-			combination := []CPlr{board[rowI][colI], board[rowI+1][colI+1], board[rowI+2][colI+2], board[rowI+3][colI+3]}
-			combinations = append(combinations, combination)
-		}
-	}
-
-	// iterate over every diagonal (starting from top-right corner)
-	for rowI := 0; rowI < 3; rowI++ {
-		for j := 3; j < 7; j++ {
-			combination := []CPlr{board[rowI][j], board[rowI+1][j-1], board[rowI+2][j-2], board[rowI+3][j-3]}
-			combinations = append(combinations, combination)
-		}
-	}
-
-	return combinations
-}
-
-func winnableN(s []CPlr, piece CPlr, n int) bool {
-	counter := 0
-
-	for _, spot := range s {
-		switch spot {
-		case piece:
-			counter++
-			continue
-		case CNone:
-			continue
-		default: // its opponent
-			return false
-		}
-	}
-
-	return counter == n // only equal not bigger so connect 3 don't count also as connect 2
+	return number3s, number2s
 }
---
+++ game/flameAiUtil.go
@@ -0,0 +1,91 @@
+package game
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func inSlice(s []int, a int) bool {
+	for _, obj := range s {
+		if obj == a {
+			return true
+		}
+	}
+
+	return false
+}
+
+func winnableN(s []CPlr, piece CPlr, n int) bool {
+	counter := 0
+
+	for _, spot := range s {
+		switch spot {
+		case piece:
+			counter++
+			continue
+		case CNone:
+			continue
+		default: // its opponent
+			return false
+		}
+	}
+
+	return counter == n // only equal not bigger so connect 3 don't count also as connect 2
+}
+
+func getCombinations(board CBoard) [][]CPlr {
+	/*
+		[[2 0 0 0 2 2 2]
+		 [1 1 1 0 1 0 1]
+		 [1 2 2 0 0 0 2]
+		 [2 1 1 1 1 1 1]
+		 [1 0 0 2 2 2 2]
+		 [0 0 1 1 1 1 2]]
+	*/
+	var combinations [][]CPlr
+
+	// row combinations
+	for _, row := range board {
+		for colI := 0; colI < 4; colI++ {
+			combination := row[colI : colI+4]
+			combinations = append(combinations, combination)
+		}
+	}
+
+	// col combination no because they can just be blocked
+	// // col combinations
+	// for col := 0; col < CColsNum; col++ {
+	// 	for rowI := 0; rowI < 3; rowI++ {
+	// 		combination := []CPlr{board[rowI][col], board[rowI+1][col], board[rowI+2][col], board[rowI+3][col]}
+	// 		combinations = append(combinations, combination)
+	// 	}
+	// }
+
+	for rowI := 0; rowI < 3; rowI++ {
+		for colI := 0; colI < 4; colI++ {
+			combination := []CPlr{board[rowI][colI], board[rowI+1][colI+1], board[rowI+2][colI+2], board[rowI+3][colI+3]}
+			combinations = append(combinations, combination)
+		}
+	}
+
+	// iterate over every diagonal (starting from top-right corner)
+	for rowI := 0; rowI < 3; rowI++ {
+		for j := 3; j < 7; j++ {
+			combination := []CPlr{board[rowI][j], board[rowI+1][j-1], board[rowI+2][j-2], board[rowI+3][j-3]}
+			combinations = append(combinations, combination)
+		}
+	}
+
+	return combinations
+}
